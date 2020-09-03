ad8e57da735bc816934ec984e37c15823337ab2a
asokraju <kkrishnachaitanya89@gmail.com>
2020-09-03 02:16:37 -0400

    crc started to run 10 simulations

--- my_scripts/test_name=dcbf_2/gamma=0.99/time_steps=3/results/job.sh
+++ my_scripts/test_name=dcbf_2/gamma=0.99/time_steps=3/results/job.sh
@@ -9,4 +9,4 @@ module load conda
 module load cuda
 module load cudnn
 conda activate tf_gpu_krishna
-python /afs/crc.nd.edu/user/k/kkosaraj/GITHUB_BUILD_1/microgrid_dcbf.py --gamma=0.99 --time_steps=3 --summary_dir=/afs/crc.nd.edu/user/k/kkosaraj/GITHUB_BUILD_1/my_scripts/test_name=dcbf_2/gamma=0.99/time_steps=3 > out.txt
+python /afs/crc.nd.edu/user/k/kkosaraj/GITHUB_BUILD_1/hvac_cbf.py --gamma=0.99 --time_steps=3 --summary_dir=/afs/crc.nd.edu/user/k/kkosaraj/GITHUB_BUILD_1/my_scripts/test_name=dcbf_2/gamma=0.99/time_steps=3 > out.txt
