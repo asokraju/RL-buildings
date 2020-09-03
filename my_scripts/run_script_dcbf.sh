ca15b37be3b9b243b57eea8d812b3ef7caea387e
Krishna Chaitanya Kosaraju <kkrishnachaitanya89@gmail.com>
2020-09-03 02:13:55 -0400

    Update run_script_dcbf.sh

--- my_scripts/run_script_dcbf.sh
+++ my_scripts/run_script_dcbf.sh
@@ -3,7 +3,7 @@ for test_name in dcbf_2
 do
   for gamma in 0.99
   do
-    for time_steps in 1 3 5 7 8
+    for time_steps in 1 2 3 4 5 6 7 8 9 10
     do
       ./gpu_batch_dcbf.sh $test_name $gamma $time_steps
     done
